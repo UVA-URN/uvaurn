b5c530f2ce356e58a18398284cf3f394948974d2
Alexander White <67017834+adw5ke@users.noreply.github.com>
2021-09-30 18:00:21 -0400

    Update base.py

--- uvaurn/settings/base.py
+++ uvaurn/settings/base.py
@@ -160,3 +160,7 @@ WAGTAIL_SITE_NAME = "uvaurn"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
